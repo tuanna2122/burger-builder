d83d8cdb47e51dfc997de9be0a15d0a4636f0a98
tuanna2122 <tuanna2122@gmail.com>
2021-03-12 00:25:38 +0700

    toolbar

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -82,6 +82,10 @@ class BurgerBuilder extends Component {
         this.setState({ purchasing: false });
     };
 
+    purchaseContinueHandler = () => {
+        alert('You continue!');
+    };
+
     render() {
         const disabledInfo = { ...this.state.ingredients };
         for (let key in disabledInfo) {
@@ -90,7 +94,11 @@ class BurgerBuilder extends Component {
         return (
             <Auxiliary>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary ingredients={this.state.ingredients} />
+                    <OrderSummary
+                        ingredients={this.state.ingredients}
+                        purchaseCancelled={this.purchaseCancelHandler}
+                        purchaseContinued={this.purchaseContinueHandler}
+                        price={this.state.totalPrice} />
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
