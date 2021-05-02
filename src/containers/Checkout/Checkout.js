b985259e35d0e0faa967b163eae84d0d27103b70
tuanna2122 <tuanna2122@gmail.com>
2021-05-03 01:13:26 +0700

    advance redux continue

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -17,8 +17,10 @@ class Checkout extends Component {
     render() {
         let summary = <Redirect to="/" />;
         if (this.props.ings) {
+            const purchasedRedirect = this.props.purchased && <Redirect to="/" />;
             summary = (
                 <div>
+                    {purchasedRedirect}
                     <CheckoutSummary
                         ingredients={this.props.ings}
                         checkoutCancelled={this.checkoutCancelledHandler}
@@ -35,7 +37,8 @@ class Checkout extends Component {
 
 const mapStateToProps = state => {
     return {
-        ings: state.burgerBuilder.ingredients
+        ings: state.burgerBuilder.ingredients,
+        purchased: state.order.purchased
     };
 };
 
