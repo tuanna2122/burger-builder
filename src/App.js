18cd8dc8dd842571a100c827744e86aae49ec96f
tuanna2122 <tuanna2122@gmail.com>
2021-05-06 23:45:39 +0700

    local storage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -7,23 +8,34 @@ import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
+import * as actions from './store/actions/index';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <Layout>
-          <Switch>
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" component={Orders} />
-            <Route path="/auth" component={Auth} />
-            <Route path="/logout" component={Logout} />
-            <Route path="/" exact component={BurgerBuilder} />
-          </Switch>
-        </Layout>
-      </div>
-    );
-  }
+	componentDidMount() {
+		this.props.onTryAutoSignup();
+	};
+
+	render() {
+		return (
+			<div>
+				<Layout>
+					<Switch>
+						<Route path="/checkout" component={Checkout} />
+						<Route path="/orders" component={Orders} />
+						<Route path="/auth" component={Auth} />
+						<Route path="/logout" component={Logout} />
+						<Route path="/" exact component={BurgerBuilder} />
+					</Switch>
+				</Layout>
+			</div>
+		);
+	}
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+	return {
+		onTryAutoSignup: () => dispatch(actions.authCheckState())
+	};
+};
+
+export default withRouter(connect(null, mapDispatchToProps)(App));
